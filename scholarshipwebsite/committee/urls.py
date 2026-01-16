4aa2243b6ef4f18e842ec5d2600b79aa2b97f6cf
Ker Jing <jingkerchan@gmail.com>
2026-01-16 12:41:50 +0800

    edited frontend

--- scholarshipwebsite/committee/urls.py
+++ scholarshipwebsite/committee/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="committee"),
     path("manage/", views.manage, name="manage"),
+    path("create/", views.create, name="create"),
     path("reviewApprove/", views.reviewApprove, name="reviewApprove"),
+    path("schedule/", views.schedule, name="schedule"),
 ]
--- scholarshipwebsite/committee/views.py
+++ scholarshipwebsite/committee/views.py
@@ -8,5 +8,14 @@ def index(response):
 def manage(response):
     return render(response, "committee/manageScholarship.html", {})
 
+def create(response):
+    return render(response, "committee/createScholarship.html", {})
+
+def edit(response):
+    return render(response, "committee/editScholarship.html", {})
+
 def reviewApprove(response):
     return render(response, "committee/reviewApprove.html", {})
+
+def schedule(response):
+    return render(response, "committee/scheduleInterview.html", {})
